31b0caf4c7b898c8341bea55950e2f17f921f27f
ZhekaVasil <zheka_1408@mail.ru>
2016-09-08 02:10:54 +0300

    cat

--- news_del.php
+++ news_del.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Евгений
- * Date: 08.09.2016
- * Time: 1:59
- */
+require 'connect.php';
+if(isset($_POST['del'])){
+    $stmt = $conn->prepare("DELETE FROM news WHERE name = :name ");
+    $stmt->bindParam(':name', $_POST['del']);
+    $stmt->execute();
+}
+$stmt = $conn->query("SELECT name FROM news");
+?>
+<form method="post" action="">
+    <p>Delite news</p>
+    <label>Choose news: <select name="del">' ;
+            <?php while($data = $stmt->fetch()){
+            echo "<option name ='{$data['name']}' value='{$data['name']}'>{$data['name']}</option>";
+            }?>
+            </select></label>
+    <input type='submit' value='DELITE'></form>
+<?php
+unset($conn);
+?>
+<a href="news.php">Back</a>
