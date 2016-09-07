21d7e2e434631bb3baf920155c5c7ebfa902687e
ZhekaVasil <zheka_1408@mail.ru>
2016-09-08 01:48:31 +0300

    cat

---
+++ news.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PDO</title>
+</head>
+<body>
+<ul>
+    <li><a href="news_add.php">Add news</li>
+    <li><a href="news_del.php">Delite news</li>
+    <li><a href="news_edit.php">Edit news</li>
+</ul>
+<a href="index.html">Back</a>
+</body>
+</html>
