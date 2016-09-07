1f4ba8d194f8d12796a518ded1daaf05fcf4c95b
ZhekaVasil <zheka_1408@mail.ru>
2016-09-07 23:34:13 +0300

    cat

--- cat.php
+++ cat.php
@@ -6,9 +6,9 @@
 </head>
 <body>
 <ul>
-    <li><a href="cat_add.php"></li>
-    <li><a href="cat_del.php"></li>
-    <li><a href="cat_edit.php"></li>
+    <li><a href="cat_add.php">Add category</li>
+    <li><a href="cat_del.php">Delite category</li>
+    <li><a href="cat_edit.php">Edit category</li>
 </ul>
 </body>
 </html>
