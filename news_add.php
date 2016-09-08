f31c679d8a4c3e9ba3f4317dde8e89e9952d10c3
agronin.al <agronin.al@gmail.com>
2016-09-08 19:41:22 +0300

    it

--- news_add.php
+++ news_add.php
@@ -1,6 +1,15 @@
 <?php
 require 'connect.php';
 if(isset($_POST['cat_name'])){
+    $fileSRC ='';
+    if(is_uploaded_file($_FILES["filename"]["tmp_name"]))
+    {
+        $file = iconv('utf-8', 'windows-1251', $_FILES["filename"]["name"]);
+        move_uploaded_file($_FILES["filename"]["tmp_name"], "upload/".$file );
+        $fileSRC = '<img src="upload/'.$file.'">';
+    } else {
+        echo("Ошибка загрузки файла");
+    }
     $stmt = $conn->prepare("SELECT id FROM categories WHERE alt_name = :post  ");
     $stmt->bindParam(':post',$_POST['cat_name'] );
     $stmt->execute();
@@ -9,7 +18,8 @@ if(isset($_POST['cat_name'])){
     $stmt2 = $conn->prepare("INSERT INTO news (cat_id, name, text) VALUES (:cat_id, :name, :text)");
     $stmt2->bindParam(':cat_id', $id['id'] );
     $stmt2->bindParam(':name', $_POST['name'] );
-    $stmt2->bindParam(':text', $_POST['text'] );
+    $pic = $fileSRC."<br>".$_POST['text'];
+    $stmt2->bindParam(':text', $pic );
     $stmt2->execute();
 
 }
@@ -23,16 +33,18 @@ $stmt = $conn->query("SELECT alt_name FROM categories");
 </head>
 <body>
 
-<form method="post" action="">
+<form method="post" action="" enctype="multipart/form-data" accept-charset="utf-8">
     <label>Choose category: <select name='cat_name'>
             <?php while ($data = $stmt->fetch()) {
             echo "<option value='{$data['alt_name']}' name='{$data['alt_name']}'>{$data['alt_name']}</option>";
             }?>
         </select></label><br>
-    <label>Enter name: <input type='text' name='name'></label>
+    <label>Enter name: <input type='text' name='name'></label><br>
+    <input type="file" name="filename" id="file">
     <label><textarea name='text'></textarea></label>
     <input type='submit' value='ADD'>
 </form>
+
 <?php
 unset($conn);
 ?>
